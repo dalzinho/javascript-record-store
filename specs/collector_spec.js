4b4a7c1bb49a5bee8b01b1268579aeeff3c85a71
John Daly <jpdaly@gmail.com>
2017-03-25 06:21:35 +0000

    record collector spec added

---
+++ specs/collector_spec.js
@@ -0,0 +1,15 @@
+var Collector = require('../collector');
+var assert = require('assert');
+
+describe('record collector',function(){
+
+  it('has a cash balance');
+
+  it('can buy records from the store');
+
+  it('increases its balance when selling records');
+
+  it("can't buy records it can't afford");
+
+
+})
